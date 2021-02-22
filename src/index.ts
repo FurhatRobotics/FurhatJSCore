c76c467655e6ebf2ac1b1413e6c62e5d5439e20a
subbu <subbu@furhatrobotics.com>
2021-02-22 13:47:01 +0100

    Added onerror and onclose callback method registers

--- src/index.ts
+++ src/index.ts
@@ -34,12 +34,12 @@ export default class Furhat {
    * Initializes the connection and return a promise. Await for the promise to resolve before 
    * using the object
    */
-  init() {
+  init(): Promise<{error: boolean, message: string}> {
     return new Promise((resolve, reject) => {
       this.socket = new WebSocket(`ws://${this.domain}:${this.port}/${this.route}`)
 
       this.socket.onopen = () => {
-        resolve("Success")
+        resolve({error: false, message: "Success"})
       }
 
       this.socket.onmessage = (message) => {
@@ -47,16 +47,27 @@ export default class Furhat {
 
         // If a callback is available then call it
         this.eventFunctions[event.event_name] !== undefined &&
-          this.eventFunctions[event.event_name](event) 
-        
-      } 
+          this.eventFunctions[event.event_name](event)
+      }
 
       this.socket.onerror = () => {
-        reject("Error on socket")
+        reject({error: true, message: "Error while opening socket"})
       }
     })
   }
 
+  /**
+   * Method to set a callback that will be triggered `onerror` of the underlying websocket
+   * @param callback Callback function to be trigger on WebSocket connection error
+   */
+  onConnectionError(callback: (this: WebSocket, ev: globalThis.Event) => any) { this.socket && (this.socket.onerror = callback) }
+
+  /**
+   * Method to set a callback that will be triggered `onclose` of the underlying websocket
+   * @param callback Callback function to be trigger on WebSocket connection close
+   */
+  onConnectionClose(callback: (this: WebSocket, ev: globalThis.Event) => any) { this.socket && (this.socket.onclose = callback) }
+  
   /**
    * Sends an event to furhatOS
    * @param event Object containing the event. Mandtory to have event_name parameter in the object
@@ -81,55 +92,56 @@ export default class Furhat {
    * the subscribe event or not. use it to set callbacks for event that are already subscribed to,
    * for instance with group subscriptions
    */
-  subscribe(eventName: string, callback: EventCallback, dontSend: boolean = false) {
+  subscribe(eventName: string, callback: EventCallback, dontSend: boolean = false): boolean {
     const event: Event = { event_name: 'furhatos.event.actions.ActionRealTimeAPISubscribe', name: eventName }
     this.eventFunctions[eventName] = callback
     if (!dontSend) {
-      this.send(event)
+      return this.send(event)
     }
+    return true
   }
 
   /**
    * Subscribes to the given event group
    * @param groupNumber Number(Assigned ENUM) of the group that needs to be subscribed to
    */
-  subscribeGroup(groupNumber: number) {
+  subscribeGroup(groupNumber: number): boolean {
     const event: Event = { event_name: 'furhatos.event.actions.ActionRealTimeAPISubscribe', group: groupNumber }
-    this.send(event)
+    return this.send(event)
   }
 
   /**
    * Says a given text
    * @param text Text which needs to be said by Furhat
    */
-  say(text: string) {
+  say(text: string): boolean {
     const event: Event = { event_name: 'furhatos.event.actions.ActionSpeech', text }
-    this.send(event)
+    return this.send(event)
   }
 
   /**
    * Stimulates the speech of a user in the interaction space
    * @param text Text which needs to be said by the user
    */
-  userSpeech(text: string) {
+  userSpeech(text: string): boolean {
     const event: Event = { event_name: 'furhatos.event.senses.SenseTypingEnd', messageText: text }
-    this.send(event)
+    return this.send(event)
   }
 
   /**
    * Stimulates SenseSpeechStart event. Can be used to stimulate user speech via typing
    */
-  userSpeechStart() {
+  userSpeechStart(): boolean {
     const event: Event = { event_name: 'furhatos.event.senses.SenseTypingStart' }
-    this.send(event)
+    return this.send(event)
   }
 
   /**
    * Performs the given gesture
    * @param name Name of the gesture that needs to be performed
    */
-  gesture(name: string) {
+  gesture(name: string): boolean {
     const event: Event = { event_name: 'furhatos.event.actions.ActionGesture', name }
-    this.send(event)
+    return this.send(event)
   }
 }
